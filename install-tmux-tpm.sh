190d56264733f24e553b925251a52f74dbd21077
Bar Perach <barp@google.com>
2022-01-03 07:34:53 +0200

    Fix tmux tpm install script

--- install-tmux-tpm.sh
+++ install-tmux-tpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cat <<EOF > ~/.tmux.conf
+cat <<EOF >> ~/.tmux.conf
 set -g @plugin 'tmux-plugins/tpm'
 
 run '~/.tmux/plugin/tpm/tpm'

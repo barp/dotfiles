1453dc80b4221c6e6c901ba0731697bc435c9b87
Bar Perach <barp@google.com>
2022-01-14 09:42:04 +0200

    Auto reload plugins and remove unused stuff

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -4,11 +4,11 @@ local miscMap = user_map.misc
 local cmd = vim.cmd
 
 local function map(mode, lhs, rhs, opts)
-   local options = { noremap = true, silent = true }
-   if opts then
-      options = vim.tbl_extend("force", options, opts)
-   end
-   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 local M = {}
@@ -16,230 +16,215 @@ local opt = {}
 
 -- these mappings will only be called during initialization
 M.misc = function()
-   -- dont copy any deleted text , this is disabled by default so uncomment the below mappings if you want them
-   -- map("n", "dd", [=[ "_dd ]=], opt)
-   -- map("v", "dd", [=[ "_dd ]=], opt)
-   -- map("v", "x", [=[ "_x ]=], opt)
-   -- todo: this should be configurable via chadrc
+	-- dont copy any deleted text , this is disabled by default so uncomment the below mappings if you want them
+	-- map("n", "dd", [=[ "_dd ]=], opt)
+	-- map("v", "dd", [=[ "_dd ]=], opt)
+	-- map("v", "x", [=[ "_x ]=], opt)
+	-- todo: this should be configurable via chadrc
 
-   -- Don't copy the replaced text after pasting in visual mode
-   map("v", "p", '"_dP', opt)
+	-- Don't copy the replaced text after pasting in visual mode
+	map("v", "p", '"_dP', opt)
 
-   -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
-   -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
-   -- empty mode is same as using :map
-   map("", "j", 'v:count ? "j" : "gj"', { expr = true })
-   map("", "k", 'v:count ? "k" : "gk"', { expr = true })
-   map("", "<Down>", 'v:count ? "j" : "gj"', { expr = true })
-   map("", "<Up>", 'v:count ? "k" : "gk"', { expr = true })
+	-- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
+	-- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+	-- empty mode is same as using :map
+	map("", "j", 'v:count ? "j" : "gj"', { expr = true })
+	map("", "k", 'v:count ? "k" : "gk"', { expr = true })
+	map("", "<Down>", 'v:count ? "j" : "gj"', { expr = true })
+	map("", "<Up>", 'v:count ? "k" : "gk"', { expr = true })
 
-   -- lazygit
-   map("n", miscMap.open_lazygit, ":lua _LAZYGIT_TOGGLE()<CR>", opt)
+	-- lazygit
+	map("n", miscMap.open_lazygit, ":lua _LAZYGIT_TOGGLE()<CR>", opt)
 
-   -- copy whole file content
-   map("n", miscMap.copywhole_file, ":%y+<CR>", opt)
+	-- copy whole file content
+	map("n", miscMap.copywhole_file, ":%y+<CR>", opt)
 
-   -- toggle numbers
-   map("n", miscMap.toggle_linenr, ":set nu!<CR>", opt)
+	-- toggle numbers
+	map("n", miscMap.toggle_linenr, ":set nu!<CR>", opt)
 
-   -- terminals
-   local function terms()
-      local m = user_map.terms
+	-- terminals
+	local function terms()
+		local m = user_map.terms
 
-      -- get out of terminal mode
-      map("t", m.esc_termmode, "<C-\\><C-n>", opt)
-      -- hide a term from within terminal mode
-      map("t", m.esc_hide_termmode, "<C-\\><C-n> :lua require('utils').close_buffer() <CR>", opt)
-      -- pick a hidden term
-      map("n", m.pick_term, ":Telescope terms <CR>", opt)
+		-- get out of terminal mode
+		map("t", m.esc_termmode, "<C-\\><C-n>", opt)
+		-- hide a term from within terminal mode
+		map("t", m.esc_hide_termmode, "<C-\\><C-n> :lua require('utils').close_buffer() <CR>", opt)
+		-- pick a hidden term
+		map("n", m.pick_term, ":Telescope terms <CR>", opt)
 
-      -- Open terminals
-      -- TODO this opens on top of an existing vert/hori term, fixme
-      map("n", m.new_wind, ":execute 'terminal' | let b:term_type = 'wind' | startinsert <CR>", opt)
-      map("n", m.new_vert, ":execute 'vnew +terminal' | let b:term_type = 'vert' | startinsert <CR>", opt)
-      map("n", m.new_hori, ":execute 15 .. 'new +terminal' | let b:term_type = 'hori' | startinsert <CR>", opt)
-   end
-   terms()
+		-- Open terminals
+		-- TODO this opens on top of an existing vert/hori term, fixme
+		map("n", m.new_wind, ":execute 'terminal' | let b:term_type = 'wind' | startinsert <CR>", opt)
+		map("n", m.new_vert, ":execute 'vnew +terminal' | let b:term_type = 'vert' | startinsert <CR>", opt)
+		map("n", m.new_hori, ":execute 15 .. 'new +terminal' | let b:term_type = 'hori' | startinsert <CR>", opt)
+	end
+	terms()
 
-   -- ctrl + s to save file
-   map("n", "<C-s>", ":w <CR>", opt)
+	-- ctrl + s to save file
+	map("n", "<C-s>", ":w <CR>", opt)
 
-   -- use ESC to turn off search highlighting
-   map("n", "<Esc>", ":noh<CR>", opt)
+	-- use ESC to turn off search highlighting
+	map("n", "<Esc>", ":noh<CR>", opt)
 
-   -- navigation within insert mode
-   local check_insertNav = require("utils").load_config().options.enable_insertNav
+	-- navigation within insert mode
+	local check_insertNav = require("utils").load_config().options.enable_insertNav
 
-   if check_insertNav == true then
-      local m = user_map.insert_nav
+	if check_insertNav == true then
+		local m = user_map.insert_nav
 
-      map("i", m.forward, "<Right>", opt)
-      map("i", m.backward, "<Left>", opt)
-      map("i", m.top_of_line, "<ESC>^i", opt)
-      map("i", m.end_of_line, "<End>", opt)
-      map("i", m.next_line, "<Up>", opt)
-      map("i", m.prev_line, "<Down>", opt)
-   end
+		map("i", m.forward, "<Right>", opt)
+		map("i", m.backward, "<Left>", opt)
+		map("i", m.top_of_line, "<ESC>^i", opt)
+		map("i", m.end_of_line, "<End>", opt)
+		map("i", m.next_line, "<Up>", opt)
+		map("i", m.prev_line, "<Down>", opt)
+	end
 
-   -- check the theme toggler
-   local theme_toggler = require("utils").load_config().ui.theme_toggler
-   if theme_toggler == true then
-      local m = user_map.misc.theme_toggle
+	-- check the theme toggler
+	local theme_toggler = require("utils").load_config().ui.theme_toggler
+	if theme_toggler == true then
+		local m = user_map.misc.theme_toggle
 
-      map("n", m, ":lua require('utils').toggle_theme(require('utils').load_config().ui.fav_themes)<CR>", opt)
-   end
+		map("n", m, ":lua require('utils').toggle_theme(require('utils').load_config().ui.fav_themes)<CR>", opt)
+	end
 
-   -- Packer commands till because we are not loading it at startup
-   cmd "silent! command PackerCompile lua require 'pluginList' require('packer').compile()"
-   cmd "silent! command PackerInstall lua require 'pluginList' require('packer').install()"
-   cmd "silent! command PackerStatus lua require 'pluginList' require('packer').status()"
-   cmd "silent! command PackerSync lua require 'pluginList' require('packer').sync()"
-   cmd "silent! command PackerUpdate lua require 'pluginList' require('packer').update()"
+	-- Packer commands till because we are not loading it at startup
+	cmd("silent! command PackerCompile lua require 'pluginList' require('packer').compile()")
+	cmd("silent! command PackerInstall lua require 'pluginList' require('packer').install()")
+	cmd("silent! command PackerStatus lua require 'pluginList' require('packer').status()")
+	cmd("silent! command PackerSync lua require 'pluginList' require('packer').sync()")
+	cmd("silent! command PackerUpdate lua require 'pluginList' require('packer').update()")
 
-   -- add NvChadUpdate command and mapping
-   cmd "silent! command! NvChadUpdate lua require('utils').update_nvchad()"
-   map("n", user_map.misc.update_nvchad, ":NvChadUpdate<CR>", opt)
+	-- add NvChadUpdate command and mapping
+	cmd("silent! command! NvChadUpdate lua require('utils').update_nvchad()")
+	map("n", user_map.misc.update_nvchad, ":NvChadUpdate<CR>", opt)
 end
 
 M.bufferline = function()
-   local m = user_map.bufferline
+	local m = user_map.bufferline
 
-   map("n", m.new_buffer, ":enew<CR>", opt) -- new buffer
-   map("n", m.newtab, ":tabnew<CR>", opt) -- new tab
-   map("n", m.close, ":lua require('utils').close_buffer() <CR>", opt) -- close  buffer
+	map("n", m.new_buffer, ":enew<CR>", opt) -- new buffer
+	map("n", m.newtab, ":tabnew<CR>", opt) -- new tab
+	map("n", m.close, ":lua require('utils').close_buffer() <CR>", opt) -- close  buffer
 
-   -- move between tabs
+	-- move between tabs
 
-   map("n", m.cycleNext, ":BufferLineCycleNext<CR>", opt)
-   map("n", m.cyclePrev, ":BufferLineCyclePrev<CR>", opt)
+	map("n", m.cycleNext, ":BufferLineCycleNext<CR>", opt)
+	map("n", m.cyclePrev, ":BufferLineCyclePrev<CR>", opt)
 end
 
 M.chadsheet = function()
-   local m = user_map.chadsheet
+	local m = user_map.chadsheet
 
-   map("n", m.default_keys, ":lua require('cheatsheet').show_cheatsheet_telescope()<CR>", opt)
-   map(
-      "n",
-      m.user_keys,
-      ":lua require('cheatsheet').show_cheatsheet_telescope{bundled_cheatsheets = false, bundled_plugin_cheatsheets = false }<CR>",
-      opt
-   )
+	map("n", m.default_keys, ":lua require('cheatsheet').show_cheatsheet_telescope()<CR>", opt)
+	map(
+		"n",
+		m.user_keys,
+		":lua require('cheatsheet').show_cheatsheet_telescope{bundled_cheatsheets = false, bundled_plugin_cheatsheets = false }<CR>",
+		opt
+	)
 end
 
-M.dap = function ()
-  local m = user_map.dap
-  map("n", m.continue, ":lua require'dap'.continue()<CR>", opt)
-  map("n", m.step_over, ":lua require'dap'.step_over()<CR>", opt)
-  map("n", m.step_into, ":lua require'dap'.step_into()<CR>", opt)
-  map("n", m.step_out, ":lua require'dap'.step_out()<CR>", opt)
-  map("n", m.toggle_breakpoint, ":lua require'dap'.toggle_breakpoint()<CR>", opt)
-  map("n", m.set_conditional_breakpoint, ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opt)
-  map("n", m.set_logpoint_message, ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", opt)
-  map("n", m.repl_open, ":lua require'dap'.repl.open()<CR>", opt)
-  map("n", m.run_last, ":lua require'dap'.run_last()<CR>", opt)
-  map("n", m.go_test_debug, ":lua require'dap-go'.debug_test()<CR>", opt)
-  map("n", m.eval_expression, ":lua require'dapui'.eval()<CR>", opt)
-  map("n", m.ui_toggle, ":lua require'dapui'.toggle()<CR>", opt)
-  map("n", m.sonic_debug_test, ":lua require'plugins.dapconf'.trigger_sonic()<CR>", opt)
+M.dap = function()
+	local m = user_map.dap
+	map("n", m.continue, ":lua require'dap'.continue()<CR>", opt)
+	map("n", m.step_over, ":lua require'dap'.step_over()<CR>", opt)
+	map("n", m.step_into, ":lua require'dap'.step_into()<CR>", opt)
+	map("n", m.step_out, ":lua require'dap'.step_out()<CR>", opt)
+	map("n", m.toggle_breakpoint, ":lua require'dap'.toggle_breakpoint()<CR>", opt)
+	map(
+		"n",
+		m.set_conditional_breakpoint,
+		":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+		opt
+	)
+	map(
+		"n",
+		m.set_logpoint_message,
+		":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+		opt
+	)
+	map("n", m.repl_open, ":lua require'dap'.repl.open()<CR>", opt)
+	map("n", m.run_last, ":lua require'dap'.run_last()<CR>", opt)
+	map("n", m.go_test_debug, ":lua require'dap-go'.debug_test()<CR>", opt)
+	map("n", m.eval_expression, ":lua require'dapui'.eval()<CR>", opt)
+	map("n", m.ui_toggle, ":lua require'dapui'.toggle()<CR>", opt)
+	map("n", m.sonic_debug_test, ":lua require'plugins.dapconf'.trigger_sonic()<CR>", opt)
 end
 
 M.comment_nvim = function()
-   local m = user_map.comment_nvim.comment_toggle
-   map("n", m, ":CommentToggle<CR>", opt)
-   map("v", m, ":CommentToggle<CR>", opt)
+	local m = user_map.comment_nvim.comment_toggle
+	map("n", m, ":CommentToggle<CR>", opt)
+	map("v", m, ":CommentToggle<CR>", opt)
 end
 
 M.dashboard = function()
-   local m = user_map.dashboard
+	local m = user_map.dashboard
 
-   map("n", m.open, ":Dashboard<CR>", opt)
-   map("n", m.newfile, ":DashboardNewFile<CR>", opt)
-   map("n", m.bookmarks, ":DashboardJumpMarks<CR>", opt)
-   map("n", m.sessionload, ":SessionLoad<CR>", opt)
-   map("n", m.sessionsave, ":SessionSave<CR>", opt)
+	map("n", m.open, ":Dashboard<CR>", opt)
+	map("n", m.newfile, ":DashboardNewFile<CR>", opt)
+	map("n", m.bookmarks, ":DashboardJumpMarks<CR>", opt)
+	map("n", m.sessionload, ":SessionLoad<CR>", opt)
+	map("n", m.sessionsave, ":SessionSave<CR>", opt)
 end
 
 M.fugitive = function()
-   local m = user_map.fugitive
+	local m = user_map.fugitive
 
-   map("n", m.Git, ":Git<CR>", opt)
-   map("n", m.diffget_2, ":diffget //2<CR>", opt)
-   map("n", m.diffget_3, ":diffget //3<CR>", opt)
-   map("n", m.git_blame, ":Git blame<CR>", opt)
+	map("n", m.Git, ":Git<CR>", opt)
+	map("n", m.diffget_2, ":diffget //2<CR>", opt)
+	map("n", m.diffget_3, ":diffget //3<CR>", opt)
+	map("n", m.git_blame, ":Git blame<CR>", opt)
 end
 
 M.nvimtree = function()
-   local m = user_map.nvimtree.treetoggle
+	local m = user_map.nvimtree.treetoggle
 
-   map("n", m, ":NvimTreeToggle<CR>", opt)
+	map("n", m, ":NvimTreeToggle<CR>", opt)
 end
 
 M.neoformat = function()
-   local m = user_map.neoformat.format
-   map("n", m, ":Neoformat<CR>", opt)
+	local m = user_map.neoformat.format
+	map("n", m, ":Neoformat<CR>", opt)
 end
 
 M.truezen = function()
-   local m = user_map.truezen
+	local m = user_map.truezen
 
-   map("n", m.ataraxisMode, ":TZAtaraxis<CR>", opt)
-   map("n", m.minimalisticmode, ":TZMinimalist<CR>", opt)
-   map("n", m.focusmode, ":TZFocus<CR>", opt)
+	map("n", m.ataraxisMode, ":TZAtaraxis<CR>", opt)
+	map("n", m.minimalisticmode, ":TZMinimalist<CR>", opt)
+	map("n", m.focusmode, ":TZFocus<CR>", opt)
 end
 
 M.telescope = function()
-   local m = user_map.telescope
+	local m = user_map.telescope
 
-   map("n", m.live_grep, ":Telescope live_grep<CR>", opt)
-   map("n", m.git_status, ":Telescope git_status <CR>", opt)
-   map("n", m.git_commits, ":Telescope git_commits <CR>", opt)
-   map("n", m.find_files, ":Telescope find_files <CR>", opt)
-   map("n", m.buffers, ":Telescope buffers<CR>", opt)
-   map("n", m.help_tags, ":Telescope help_tags<CR>", opt)
-   map("n", m.oldfiles, ":Telescope oldfiles<CR>", opt)
-   map("n", m.themes, ":Telescope themes<CR>", opt)
-end
-
-M.coc = function()
-    local m = user_map.coc
-    local plugopts = {silent = false, noremap = false}
-
-    map("n", m.definition, "<Plug>(coc-definition)", vim.tbl_extend("force", opt, plugopts))
-    map("n", m.type_definition, "<Plug>(coc-type-definition)", vim.tbl_extend("force", opt, plugopts))
-    map("n", m.references, ":lua require'telescope'.extensions.coc.references_used(require'telescope.themes'.get_cursor({path_display = {'shorten'}, layout_config={width=150}}))<CR>", opt)
-    map("n", m.implementation, ":lua require'telescope'.extensions.coc.implementations(require'telescope.themes'.get_cursor({path_display = {'shorten'}, layout_config={width=150}}))<CR>", opt)
-    map("n", m.goto_test, ":CocCommand go.test.toggle<CR>", opt)
-
-    map("n", m.diagnostic_prev, "<Plug>(coc-diagnostic-prev)", vim.tbl_extend("force", opt, plugopts))
-    map("n", m.diagnostic_next, "<Plug>(coc-diagnostic-next)", vim.tbl_extend("force", opt, plugopts))
-    map("n", m.rename, "<Plug>(coc-rename)", vim.tbl_extend("force", opt, plugopts))
-    map("n", m.format_selected, "<Plug>(coc-format-selected)", vim.tbl_extend("force", opt, plugopts))
-    map("x", m.format_selected, "<Plug>(coc-format-selected)", vim.tbl_extend("force", opt, plugopts))
-    map("n", m.diagnostics, ":CocDiagnostics<CR>", opt)
-end
-
-M.telescope_coc = function()
-    local m = user_map.telescope_coc
-
-    map("n", m.document_symbols, ":Telescope coc document_symbols<CR>")
-    map("n", m.workspace_symbols, ":Telescope coc workspace_symbols<CR>")
+	map("n", m.live_grep, ":Telescope live_grep<CR>", opt)
+	map("n", m.git_status, ":Telescope git_status <CR>", opt)
+	map("n", m.git_commits, ":Telescope git_commits <CR>", opt)
+	map("n", m.find_files, ":Telescope find_files <CR>", opt)
+	map("n", m.buffers, ":Telescope buffers<CR>", opt)
+	map("n", m.help_tags, ":Telescope help_tags<CR>", opt)
+	map("n", m.oldfiles, ":Telescope oldfiles<CR>", opt)
+	map("n", m.themes, ":Telescope themes<CR>", opt)
 end
 
 M.lsp = function()
-    local m = user_map.telescope_coc
+	local m = user_map.telescope_coc
 
-    map("n", m.document_symbols, ":Telescope lsp_document_symbols<CR>")
-    map("n", m.workspace_symbols, ":Telescope lsp_dynamic_workspace_symbols<CR>")
+	map("n", m.document_symbols, ":Telescope lsp_document_symbols<CR>")
+	map("n", m.workspace_symbols, ":Telescope lsp_dynamic_workspace_symbols<CR>")
 end
 
 M.telescope_media = function()
-   local m = user_map.telescope_media
-   map("n", m.media_files, ":Telescope media_files <CR>", opt)
+	local m = user_map.telescope_media
+	map("n", m.media_files, ":Telescope media_files <CR>", opt)
 end
 
-M.hop = function ()
-  local m = user_map.hop
-  map("n", m.jump_word, ":HopWord<CR>", opt)
+M.hop = function()
+	local m = user_map.hop
+	map("n", m.jump_word, ":HopWord<CR>", opt)
 end
 
 return M
--- nvim/.config/nvim/lua/pluginList.lua
+++ nvim/.config/nvim/lua/pluginList.lua
@@ -9,6 +9,13 @@ else
 	return false
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost pluginList.lua source <afile> | PackerSync
+  augroup END
+]])
+
 local use = packer.use
 
 return packer.startup(function()

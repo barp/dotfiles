32bd0f82f63ec6d145d03d3c812b91493560800d
Bar Perach <bar@aironworks.com>
2024-01-13 11:18:36 +0200

    redo debian install script, only install necesseray packages

--- 0-packages.debian.sh
+++ 0-packages.debian.sh
@@ -20,46 +20,6 @@ cargo install alacritty
 popd
 rm -rf alacritty
 
-# --------------------------
-# Install ksuperkey
-# --------------------------
-
-git clone https://github.com/hanschen/ksuperkey.git
-pushd ksuperkey
-make
-sudo make install
-popd
-rm -rf ksuperkey
-
-# --------------------------
-# Install networkmanager_dmenu
-# --------------------------
-
-git clone https://github.com/firecat53/networkmanager-dmenu.git
-pushd networkmanager-dmenu
-sudo cp networkmanager_dmenu /usr/local/bin/
-sudo chmod +x /usr/local/bin/networkmanager_dmenu
-popd
-rm -rf networkmanager-dmenu
-
-# --------------------------
-# Install xfce-polkit
-# --------------------------
-
-sudo apt-get install libxfce4ui-2-dev  libglib2.0-dev libpolkit-agent-1-dev
-git clone https://github.com/ncopa/xfce-polkit.git
-pushd xfce-polkit
-mkdir build
-cd build
-meson --prefix=/usr ..
-ninja
-sudo ninja install
-popd
-rm -rf xfce-polkit
-
-# --------------------------
-# Install nerd fonts
-# --------------------------
 
 git clone https://github.com/ryanoasis/nerd-fonts.git
 pushd nerd-fonts
@@ -72,10 +32,9 @@ rm -rf nerd-fonts
 # Install packages
 # --------------------------
 
-sudo apt-get install rofi bspwm sxhkd dunst xfce4-power-manager \
-  network-manager x11-server-utils \
-  xsettingsd bmon mpc alsa-utils xsettingsd ranger thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman \
-  nitrogen light polybar mpd python3-pip playerctl picom stow plank openbox-menu openbox
+sudo apt-get install rofi \
+  bmon mpc ranger \
+  nitrogen light mpd python3-pip stow 
 
 # Will take care of later
 # betterlockscreen

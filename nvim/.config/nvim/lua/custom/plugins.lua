dd3c5fcb5af316951e67d618dac67db14417e2e8
Bar Perach <bar@aironworks.com>
2024-08-19 22:59:14 +0900

    enable copilot

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -13,6 +13,19 @@ local plugins = {
     end, -- Override to setup mason-lspconfig
   },
 
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+      -- The mapping is set to other key, see custom/lua/mappings
+      -- or run <leader>ch to see copilot mapping sections
+    end
+  },
+
   -- override plugin configs
   {
     "williamboman/mason.nvim",

e7a72051ef27b93c41bdc528da5f4b47d2c5f406
Bar Perach <bar@aironworks.com>
2024-08-21 16:56:07 +0900

    enable autoformatting on save

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -41,3 +41,12 @@ lspconfig.tailwindcss.setup({
     },
   },
 })
+
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  group = 'AutoFormatting',
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
+})

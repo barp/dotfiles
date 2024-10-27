b8893130de7cf5feb62cecacec4624eb2ac34306
Bar Perach <bar@aironworks.com>
2024-10-27 09:30:47 +0900

    add protobuf ls

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "cssls", "tsserver", "clangd", "terraformls", "pyright", "zls", "templ", "elmls", "svelte"}
+local servers = { "cssls", "tsserver", "clangd", "terraformls", "pyright", "zls", "templ", "elmls", "svelte", "pbls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
